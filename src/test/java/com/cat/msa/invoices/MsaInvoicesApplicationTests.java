c9506202df28cebe084a4a6b4c4a24f07405d620
Mateo Bravo <mateo.bravo.f@gmail.com>
2025-05-05 18:38:51 -0500

    Init project

---
+++ src/test/java/com/cat/msa/invoices/MsaInvoicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cat.msa.invoices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsaInvoicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
