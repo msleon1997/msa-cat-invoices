1db4c1c68f48a66f19709fc4ff2ca40ae1862b2f
Mateo-Bravo <mateo.bravo.f@gmail.com>
2025-05-07 18:10:20 -0500

    feat: create entities of domain for invoices

---
+++ src/main/java/com/cat/msa/invoices/domain/InvoiceDetail.java
@@ -0,0 +1,26 @@
+package com.cat.msa.invoices.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+@Getter
+@Setter
+public class InvoiceDetail {
+
+    private Long id;
+
+    private String productName;
+
+    private Integer quantity;
+
+    private BigDecimal unitPrice;
+
+    private BigDecimal subTotal;
+
+    public void calculateSubTotal(){
+        subTotal = unitPrice.multiply(new BigDecimal(quantity));
+
+    }
+}
