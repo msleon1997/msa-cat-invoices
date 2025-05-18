2f2e0984f3d22c12743c19fc020a4133ead51e66
Mateo-Bravo <mateo.bravo.f@gmail.com>
2025-05-18 15:39:25 -0500

    feat: Detail find by id and find all

--- src/main/java/com/cat/msa/invoices/controller/api/InvoiceDetailApi.java
+++ src/main/java/com/cat/msa/invoices/controller/api/InvoiceDetailApi.java
@@ -2,13 +2,18 @@ package com.cat.msa.invoices.controller.api;
 
 import com.cat.msa.invoices.domain.InvoiceDetail;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RequestMapping("/api/v1/invoice-details")
 public interface InvoiceDetailApi {
 
     @PostMapping
     ResponseEntity<InvoiceDetail> save(@RequestBody InvoiceDetail invoiceDetail);
+
+    @GetMapping
+    ResponseEntity<List<InvoiceDetail>> findAll();
+    @GetMapping("/{id}")
+    ResponseEntity<InvoiceDetail> findById(@PathVariable Long id);
 }
--- src/main/java/com/cat/msa/invoices/service/impl/InvoiceDetailServiceImpl.java
+++ src/main/java/com/cat/msa/invoices/service/impl/InvoiceDetailServiceImpl.java
@@ -1,9 +1,13 @@
 package com.cat.msa.invoices.service.impl;
 
 import com.cat.msa.invoices.domain.InvoiceDetail;
+import com.cat.msa.invoices.domain.InvoiceHeader;
 import com.cat.msa.invoices.repository.InvoiceDetailRepository;
 import com.cat.msa.invoices.service.InvoiceDetailService;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class InvoiceDetailServiceImpl implements InvoiceDetailService {
@@ -18,5 +22,16 @@ public class InvoiceDetailServiceImpl implements InvoiceDetailService {
     public InvoiceDetail createInvoiceDetail(InvoiceDetail invoiceDetail) {
         invoiceDetail.calculateSubTotal();
         return invoiceDetailRepository.save(invoiceDetail);
+    }
+
+    @Override
+    public List<InvoiceDetail> findAll() {
+        return invoiceDetailRepository.findAll();
+    }
+
+    @Override
+    public InvoiceDetail getInvoiceDetailById(Long id) {
+        Optional<InvoiceDetail> optionalInvoiceDetail = invoiceDetailRepository.findById(id);
+        return optionalInvoiceDetail.orElse(null);
     }
 }
